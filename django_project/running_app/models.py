5b0d0792d9cd42d861bd3969966279bd8cd5407b
JohannesFriedrich <johannes.friedrich@posteo.de>
2024-10-15 22:40:02 +0200

    back to 3 cards per row

--- django_project/running_app/models.py
+++ django_project/running_app/models.py
@@ -3,10 +3,10 @@ from django.db import models
     
 class Distance(models.Model):
     name = models.CharField(max_length=200, null =True)
-    distance = models.DecimalField(default = 0, max_digits=9, decimal_places=3, null=True, blank =True)
+    distance = models.DecimalField(default = None, max_digits=9, decimal_places=3, blank=True)
 
-    def __str__ (self):
-        return self.name
+    def __str__ (self) -> str:
+        return str(self.name)
     
     class Meta:
         ordering = ['distance']  # Standardmäßige Sortierung nach Datum (aufsteigend)
